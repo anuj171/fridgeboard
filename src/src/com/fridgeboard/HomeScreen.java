daa027a907869d6d151c989829778b5b6334a0dd
tharking <kamal.choudhary@gmail.com>
2014-01-24 04:32:20 +0530

    Added support for next/prev day navigation

--- src/src/com/fridgeboard/HomeScreen.java
+++ src/src/com/fridgeboard/HomeScreen.java
@@ -7,16 +7,21 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class HomeScreen extends Activity {
 
     private ListView listView1;
+	Calendar rightNow;
+	DateFormat df;
+	private TextView mealPlanHeader;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,9 +38,9 @@ public class HomeScreen extends Activity {
             new Meal(R.drawable.ic_launcher, "Wine", "Description description Description description Description description...", "Time: 5 Min"),
         };
         
-		Calendar rightNow = Calendar.getInstance();
+		rightNow = Calendar.getInstance();
 		
-		DateFormat df = new SimpleDateFormat("EEE, d MMM");
+		df = new SimpleDateFormat("EEE, d MMM");
 		
        
         MealPlanAdapter adapter = new MealPlanAdapter(this, 
@@ -46,14 +51,72 @@ public class HomeScreen extends Activity {
          
         View header = (View)getLayoutInflater().inflate(R.layout.home_screen_header, null);
         
-        TextView mealPlanHeader = (TextView)header.findViewById(R.id.txtHeader);
+        mealPlanHeader = (TextView)header.findViewById(R.id.txtHeader);
         mealPlanHeader.setText(df.format(rightNow.getTime()));
         
+//        Button nextDate = (Button) findViewById(R.id.buttonNext);
+//        nextDate.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Do something in response to button click
+//                TextView mealPlanHeader = (TextView)v.findViewById(R.id.txtHeader);
+//                rightNow.add(Calendar.DAY_OF_MONTH, 1);
+//                mealPlanHeader.setText(df.format(rightNow.getTime()));
+//                v.invalidate();
+//            }
+//        });
+//
+//        Button prevDate = (Button) findViewById(R.id.buttonPrevious);
+//        prevDate.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Do something in response to button click
+//                TextView mealPlanHeader = (TextView)v.findViewById(R.id.txtHeader);
+//                rightNow.add(Calendar.DAY_OF_MONTH, -1);
+//                mealPlanHeader.setText(df.format(rightNow.getTime()));
+//                v.invalidate();
+//            }
+//        });
+
         listView1.addHeaderView(header);
         
         listView1.setAdapter(adapter);
     }
     
+    /** Called when the user touches the button */
+    public void nextDay(View view) {
+    	rightNow.add(Calendar.DAY_OF_MONTH, 1);
+    	mealPlanHeader.setText(df.format(rightNow.getTime()));
+    	mealPlanHeader.invalidate();
+
+//    	// Do something in response to button click
+//		new AlertDialog.Builder(this)
+//	    .setTitle("Next Day")
+//	    .setMessage(df.format(rightNow.getTime()))
+//	    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//	        public void onClick(DialogInterface dialog, int which) { 
+//	            // continue with delete
+//	        }
+//	     })
+//	    .show();    
+	}
+    
+    /** Called when the user touches the button */
+    public void previousDay(View view) {
+        // Do something in response to button click
+    	rightNow.add(Calendar.DAY_OF_MONTH, -1);
+    	mealPlanHeader.setText(df.format(rightNow.getTime()));
+    	mealPlanHeader.invalidate();
+
+//    	// Do something in response to button click
+//		new AlertDialog.Builder(this)
+//	    .setTitle("Previous Day")
+//	    .setMessage(df.format(rightNow.getTime()))
+//	    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//	        public void onClick(DialogInterface dialog, int which) { 
+//	            // continue with delete
+//	        }
+//	     })
+//	    .show();    
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,27 +129,29 @@ public class HomeScreen extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item)	{
 		switch(item.getItemId()) {
 		case R.id.action_groceries:
+			startActivity(new Intent(this, Groceries.class));
 			//Toast.makeText(this, "Ordering on Big Basket", Toast.LENGTH_SHORT).show();
-			new AlertDialog.Builder(this)
-		    .setTitle("Generate Grocery List")
-		    .setMessage("Generating groceries list for your meal plan.")
-		    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
-		            // continue with delete
-		        }
-		     })
-		    .show();
+//			new AlertDialog.Builder(this)
+//		    .setTitle("Generate Grocery List")
+//		    .setMessage("Generating groceries list for your meal plan.")
+//		    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//		        public void onClick(DialogInterface dialog, int which) { 
+//		            // continue with delete
+//		        }
+//		     })
+//		    .show();
 			break;
 		case R.id.action_settings:
-			new AlertDialog.Builder(this)
-		    .setTitle("Meal Preferences")
-		    .setMessage("using default settings.")
-		    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-		        public void onClick(DialogInterface dialog, int which) { 
-		            // continue with delete
-		        }
-		     })
-		    .show();
+			startActivity(new Intent(this, DatabaseActivity.class));
+//			new AlertDialog.Builder(this)
+//		    .setTitle("Meal Preferences")
+//		    .setMessage("using default settings.")
+//		    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//		        public void onClick(DialogInterface dialog, int which) { 
+//		            // continue with delete
+//		        }
+//		     })
+//		    .show();
 			break;
 		}
 		return false;
