4997c195142ab5842b0a5135523f1f37fcc72209
tharking <kamal.choudhary@gmail.com>
2014-01-24 03:40:41 +0530

    change button background

--- src/src/com/fridgeboard/Meal.java
+++ src/src/com/fridgeboard/Meal.java
@@ -1,16 +1,24 @@
 package com.fridgeboard;
 
 public class Meal {
-    public int icon;
+    public boolean is_category;
+	public int icon;
     public String title;
     public String desc;
     public String duration;
     public Meal(){
         super();
     }
-    
+
+    public Meal(String category) {
+        super();
+        this.is_category = true;
+        this.title = title;
+    }
+
     public Meal(int icon, String title, String desc, String duration) {
         super();
+        this.is_category = false;
         this.icon = icon;
         this.title = title;
         this.desc = desc;
