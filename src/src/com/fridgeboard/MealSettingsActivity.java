07a608dc0089bb03b104494557cd6a8d84cb649a
anuj <andhawan@microsoft.com>
2014-01-28 17:10:35 +0530

    remove setting action button from settings screen

--- src/src/com/fridgeboard/MealSettingsActivity.java
+++ src/src/com/fridgeboard/MealSettingsActivity.java
@@ -91,7 +91,7 @@ public class MealSettingsActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.meal_settings, menu);
+		//getMenuInflater().inflate(R.menu.meal_settings, menu);
 		return true;
 	}
 
