7c98ba607a5d78ef954905f948d5563868e93dd0
anuj <andhawan@microsoft.com>
2014-01-28 13:44:41 +0530

    updated db version

--- src/src/com/fridgeboard/DataAccess.java
+++ src/src/com/fridgeboard/DataAccess.java
@@ -13,7 +13,7 @@ import android.util.Log;
 
 public class DataAccess {
 	private static final String DATABASE_NAME = "meals_database.db";
-	private static final int DATABASE_VERSION = 21;
+	private static final int DATABASE_VERSION = 22;
 	
 	  public enum RecipeCategory{
 		  BreakFast,
