ff4f941d889859e3c6dc35af64ae5226e6e1b018
sanky29288 <sanky29288@gmail.com>
2014-01-28 23:51:19 +0530

    bug fixes

--- src/src/com/fridgeboard/RecipeListAdapter.java
+++ src/src/com/fridgeboard/RecipeListAdapter.java
@@ -49,7 +49,7 @@ public class RecipeListAdapter extends ArrayAdapter<RecipeItem> {
             holder.desc = (TextView)row.findViewById(R.id.txtDescSearch);
             holder.duration = (TextView)row.findViewById(R.id.txtDurationSearch);
             holder.nutrition = (TextView)row.findViewById(R.id.txtNutritionSearch);
-            
+            holder.imageTimeTaken = (ImageView)row.findViewById(R.id.imageTimeTakenSearch);
             row.setTag(holder);
         }
         else
@@ -59,15 +59,67 @@ public class RecipeListAdapter extends ArrayAdapter<RecipeItem> {
 
         holder.title.setText(item.getName());
         holder.desc.setText(item.getDescription());
-        holder.duration.setText("Time: "+ item.getTotalTime());
-        holder.nutrition.setText(item.getNutrition());
+        holder.duration.setText(item.getTotalTime());
+        holder.nutrition.setText("Nutrition: " + item.getNutrition());
         holder.image.setImageResource(
         		context.getResources().getIdentifier(
     				item.getImage(), 
     				"drawable", context.getApplicationContext().getPackageName()));
+        holder.imageTimeTaken.setImageResource(getClockImageFromTime(item.getTotalTime()));
         
         return row;
 	}
+	int getClockImageFromTime(String time_string){
+		int time = (int) Integer.parseInt(time_string.substring(0, time_string.length()-5));
+		if (time <= 5){
+			return context.getResources().getIdentifier(
+    				"clock05", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 10){
+			return context.getResources().getIdentifier(
+    				"clock10", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 15){
+			return context.getResources().getIdentifier(
+    				"clock15", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 20){
+			return context.getResources().getIdentifier(
+    				"clock20", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 25){
+			return context.getResources().getIdentifier(
+    				"clock25", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 30){
+			return context.getResources().getIdentifier(
+    				"clock30", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 35){
+			return context.getResources().getIdentifier(
+    				"clock35", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 40){
+			return context.getResources().getIdentifier(
+    				"clock40", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 45){
+			return context.getResources().getIdentifier(
+    				"clock45", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 50){
+			return context.getResources().getIdentifier(
+    				"clock50", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		} else if (time <= 55){
+			return context.getResources().getIdentifier(
+    				"clock55", 
+    				"drawable", context.getApplicationContext().getPackageName());
+		}  
+		return context.getResources().getIdentifier(
+				"clock60", 
+				"drawable", context.getApplicationContext().getPackageName());
+		}
 	
 	public class RecipeHolder{
 		ImageView image;
@@ -75,5 +127,6 @@ public class RecipeListAdapter extends ArrayAdapter<RecipeItem> {
 		TextView desc;
 		TextView duration;
 		TextView nutrition;
+		ImageView imageTimeTaken;
 	}
 }
